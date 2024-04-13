99332ee374c0d8ac86ead50e73076c6a7f94b1eb
brunnossanttos <brunossantosti99@gmail.com>
2024-04-13 02:55:13 -0300

    feat: :sparkles: develop

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+const setupSwagger = (app: INestApplication) => {
+  const options = new DocumentBuilder()
+    .setTitle('Libri API')
+    .setDescription('The Libri API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  setupSwagger(app);
+
   await app.listen(3000);
 }
+
 bootstrap();
